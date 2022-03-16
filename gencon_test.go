b1313596eeb20146c78396b0ca7f89b7b941425f
ari1021 <o.pe.3.vndt.ku@gmail.com>
2022-03-16 18:10:24 +0900

    feat: skeleton init

---
+++ gencon_test.go
@@ -0,0 +1,15 @@
+package gencon_test
+
+import (
+	"testing"
+
+	"github.com/ari1021/gencon"
+	"github.com/gostaticanalysis/testutil"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+// TestAnalyzer is a test for Analyzer.
+func TestAnalyzer(t *testing.T) {
+	testdata := testutil.WithModules(t, analysistest.TestData(), nil)
+	analysistest.Run(t, testdata, gencon.Analyzer, "a")
+}
